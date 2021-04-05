fd8a913c8d95d8073d1c75b915b958b21099e022
monicamun <47000707+monicamun@users.noreply.github.com>
2021-04-04 20:21:56 -0700

    added async and await instead of then to videoService. filter to videos only

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,34 +1,37 @@
 export function getVideos(setVideos) {
-  window.gapi.load('client:auth2', function () {
-    window.gapi.auth2
-      .init({
-        client_id:
-          '537606572960-o8ms8f2g27kepgh2fmd14m0bgsmsh6og.apps.googleusercontent.com',
+  // initialize gapi
+  window.gapi.load('client:auth2', async function () {
+    await window.gapi.auth2.init({
+      client_id:
+        '537606572960-o8ms8f2g27kepgh2fmd14m0bgsmsh6og.apps.googleusercontent.com',
+    });
+    // to initialize youtube we need to set the api key first
+    window.gapi.client.setApiKey('AIzaSyCykzCobNVjS-XeWrXWwPH06nLtq7Assis');
+    await window.gapi.client.load(
+      'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
+    );
+
+    // after initializing we can make the api call
+    // call youtube search
+    const response = await window.gapi.client.youtube.search.list({
+      part: ['snippet'],
+      maxResults: 25,
+      q: 'surfing',
+    });
+
+    const videos = response.result.items
+      .map((video) => {
+        return {
+          etag: video.etag,
+          videoId: video.id.videoId,
+          title: video.snippet.title,
+          imageUrl: video.snippet.thumbnails.medium.url,
+          description: video.snippet.description,
+        };
       })
-      .then(() => {
-        window.gapi.client.setApiKey('AIzaSyCykzCobNVjS-XeWrXWwPH06nLtq7Assis');
-        window.gapi.client
-          .load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest')
-          .then(() => {
-            window.gapi.client.youtube.search
-              .list({
-                part: ['snippet'],
-                maxResults: 25,
-                q: 'surfing',
-              })
-              .then((response) => {
-                setVideos(
-                  response.result.items.map((video) => {
-                    return {
-                      etag: video.etag,
-                      title: video.snippet.title,
-                      imageUrl: video.snippet.thumbnails.medium.url,
-                      description: video.snippet.description,
-                    };
-                  })
-                );
-              });
-          });
-      });
+      .filter((video) => video.videoId);
+
+    // use the setVideos function passed by the component to set the videos to the state
+    setVideos(videos);
   });
 }
